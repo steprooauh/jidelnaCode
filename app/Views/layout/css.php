6e58cbf7755d6bf142e7ed200c258ea42c4f838a
steprooauh <prokop_stepan@oauh.cz>
2025-10-24 10:50:26 +0200

    5416

--- app/Views/layout/css.php
+++ app/Views/layout/css.php
@@ -1,6 +1,6 @@
-<link rel="stylesheet" href="node_modules/bootswatch/dist/sandstone/bootstrap.min.css">
-<link rel="stylesheet" href="node_modules/flag-icons/css/flag-icons.min.css">
-<link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css">
-<link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
-<link rel="icon" href="img/image.png">
-<link rel="stylesheet" href="css/style1.css">
+<link rel="stylesheet" href="<?= base_url('node_modules/bootswatch/dist/sandstone/bootstrap.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('node_modules/flag-icons/css/flag-icons.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('node_modules/@fortawesome/fontawesome-free/css/all.min.css'); ?>">
+<link rel="stylesheet" href="<?= base_url('node_modules/bootstrap-icons/font/bootstrap-icons.min.css'); ?>">
+<link rel="icon" href="<?= base_url('img/image.png'); ?>">
+<link rel="stylesheet" href="<?= base_url('css/style1.css'); ?>">
